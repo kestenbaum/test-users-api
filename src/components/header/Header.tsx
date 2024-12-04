4d90c747ae6ce504232a90f5c51980234857a975
Oleksii Vovnenko <kespa71for@gmail.com>
2024-12-04 04:58:05 +0100

    add header

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,14 @@
+import {FC} from 'react'
+import style from "../header/Header.module.css";
+
+const Header:FC = () => {
+  return (
+    <header className={style.header}>
+        <div className="container">
+            
+        </div>
+    </header>
+  )
+}
+
+export default Header
