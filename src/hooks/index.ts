b9d537751dfa280db253f87df8fcbf9c997ab2a8
vovne <kespa71for@gmail.com>
2025-06-22 00:10:30 +0200

    create export file for hooks:

---
+++ src/hooks/index.ts
@@ -0,0 +1,11 @@
+import {useGetUsers} from "./useGetUsers.ts";
+import {useDebounce} from "./useDebounce.ts";
+import {useUserFilter} from "./useUserFilter.ts";
+import {useUserSort} from "./useUserSortOrder.ts";
+
+export {
+    useGetUsers,
+    useDebounce,
+    useUserSort,
+    useUserFilter,
+}
